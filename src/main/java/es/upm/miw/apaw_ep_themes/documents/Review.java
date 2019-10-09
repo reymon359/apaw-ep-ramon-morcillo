4e0674ed3f84d2c82ba6cafd477f0c88855c85bd
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-10-09 22:22:04 +0200

    POST /films Films adding more tests #5

--- src/main/java/es/upm/miw/apaw_ep_themes/documents/Review.java
+++ src/main/java/es/upm/miw/apaw_ep_themes/documents/Review.java
@@ -2,21 +2,36 @@ package es.upm.miw.apaw_ep_themes.documents;
 
 public class Review {
 
-    private String  author;
+    private String author;
 
     private String description;
 
-    public Review(String author,String description) {
-        this.author= author;
-        this.description= description;
+    public Review(String author, String description) {
+        this.author = author;
+        this.description = description;
     }
 
     public String getAuthor() {
         return author;
     }
 
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
     public String getDescription() {
         return description;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "author='" + author + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
