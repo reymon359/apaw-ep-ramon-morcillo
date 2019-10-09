07ec7d70b1916b0a52e743220ee5b171798c832d
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-10-09 19:18:18 +0200

    PUT /directors/{id} Business Controller #4

--- src/main/java/es/upm/miw/apaw_ep_themes/business_controllers/DirectorBusinessController.java
+++ src/main/java/es/upm/miw/apaw_ep_themes/business_controllers/DirectorBusinessController.java
@@ -3,6 +3,7 @@ package es.upm.miw.apaw_ep_themes.business_controllers;
 import es.upm.miw.apaw_ep_themes.daos.DirectorDao;
 import es.upm.miw.apaw_ep_themes.documents.Director;
 import es.upm.miw.apaw_ep_themes.dtos.DirectorDto;
+import es.upm.miw.apaw_ep_themes.exceptions.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -16,9 +17,22 @@ public class DirectorBusinessController {
         this.directorDao = directorDao;
     }
 
+    private Director findDirectorByIdAssured(String id) {
+        return this.directorDao.findById(id).orElseThrow(() -> new NotFoundException("Director id: " + id));
+    }
 
     public DirectorDto create(DirectorDto directorDto) {
         Director director = new Director(directorDto.getName(), directorDto.getAge(), directorDto.getAlive());
+        this.directorDao.save(director);
+        return new DirectorDto(director);
+    }
+
+    public DirectorDto update(String id, DirectorDto directorDto) {
+        directorDto.validate();
+        Director director = this.findDirectorByIdAssured(id);
+        director.setName(directorDto.getName());
+        director.setAge(directorDto.getAge());
+        director.setAlive(directorDto.getAlive());
         this.directorDao.save(director);
         return new DirectorDto(director);
     }
